0409b6d868e2f1c36bd8f767ea549727f1a6b7c6
misogi <dev@misogi.info>
2013-07-12 17:04:05 +0900

    hello world を巨大にした

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,1 +1,20 @@
-﻿printfn "Hello, World"
+﻿(*
+巨大なHello world
+Alt + Ctrl + F インタラクティブ起動
+Alt + Up / Down 並べ替え
+*)
+
+open System
+
+[<EntryPoint>]
+let main (args : String[]) = 
+    
+    if args.Length <> 2 then
+        failwith "エラー : 引数 <greeting> と <thing> が必要です"
+    
+    let greeting, thing = args.[0], args.[1]
+    let timeOfDay = DateTime.Now.ToString("hh:mm tt")
+    
+    printfn "時刻 %s : %s, %s" timeOfDay greeting thing
+    
+    0 
